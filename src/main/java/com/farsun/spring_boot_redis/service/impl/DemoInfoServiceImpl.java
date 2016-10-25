93e2f50570a8523389286b8ff2af04bc8a0fe929
hlzhua <zhuhailiang12@sina.com>
2016-10-25 15:20:32 +0800

    Demo接口的具体实现

---
+++ src/main/java/com/farsun/spring_boot_redis/service/impl/DemoInfoServiceImpl.java
@@ -0,0 +1,26 @@
+package com.farsun.spring_boot_redis.service.impl;
+
+import com.farsun.spring_boot_redis.bean.DemoInfo;
+import com.farsun.spring_boot_redis.service.DemoInfoService;
+
+public class DemoInfoServiceImpl implements DemoInfoService {
+
+	@Override
+	public DemoInfo findById(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteFromCache(long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
